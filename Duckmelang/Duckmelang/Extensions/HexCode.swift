d8f393cfe9a156ea90ff3c95c10ca5b42f6fecec
minyoy <65756020+minyoy@users.noreply.github.com>
2025-01-09 15:59:27 +0900

    Feat: 프로젝트 초기 셋팅
    Feat: 프로젝트 초기 셋팅
    
    프로젝트 생성, 라이브러리 및 Extension 셋팅, 탭바 제작

---
+++ Duckmelang/Duckmelang/Extensions/HexCode.swift
@@ -0,0 +1,40 @@
+//
+//  HexCode.swift
+//  Duckmelang
+//
+//  Created by 주민영 on 1/9/25.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    // HEX 문자열을 UIColor로 변환하는 초기화 메서드
+    convenience init?(hex: String) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+
+        var rgb: UInt64 = 0
+
+        guard Scanner(string: hexSanitized).scanHexInt64(&rgb) else { return nil }
+
+        let length = hexSanitized.count
+
+        let r, g, b, a: CGFloat
+        if length == 6 {
+            r = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
+            g = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
+            b = CGFloat(rgb & 0x0000FF) / 255.0
+            a = 1.0
+        } else if length == 8 {
+            r = CGFloat((rgb & 0xFF000000) >> 24) / 255.0
+            g = CGFloat((rgb & 0x00FF0000) >> 16) / 255.0
+            b = CGFloat((rgb & 0x0000FF00) >> 8) / 255.0
+            a = CGFloat(rgb & 0x000000FF) / 255.0
+        } else {
+            return nil
+        }
+
+        self.init(red: r, green: g, blue: b, alpha: a)
+    }
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-# duckmelang-frontend
