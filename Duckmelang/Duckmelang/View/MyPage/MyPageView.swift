9e7059c4d19a4182bfbd11fecd7ad2f38685446a
(nau) <finger9999@naver.com>
2025-01-14 02:55:15 +0900

    Feat: 뷰, 뷰컨트롤 파일 추가
    마이페이지, 남의페이지 뷰, 뷰컨트롤 파일 추가

---
+++ Duckmelang/Duckmelang/View/MyPage/MyPageView.swift
@@ -0,0 +1,20 @@
+//
+//  MyPageView.swift
+//  Duckmelang
+//
+//  Created by KoNangYeon on 1/14/25.
+//
+
+import UIKit
+
+class MyPageView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Duckmelang/Duckmelang/ViewControllers/MyPage/LoginInfoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginInfoViewController.swift
+//  Duckmelang
+//
+//  Created by KoNangYeon on 1/14/25.
+//
+
+import UIKit
+
+class LoginInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
