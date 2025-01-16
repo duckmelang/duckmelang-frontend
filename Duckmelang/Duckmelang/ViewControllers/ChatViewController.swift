196b3bab8d537f667360c878c78eb6aeff671a8e
minyoy <65756020+minyoy@users.noreply.github.com>
2025-01-16 16:14:50 +0900

    Feat: 채팅 메인 UI 구현
    Feat: 채팅 메인 UI 구현
    
    채팅 메인 UI를 구현했으며, 칩버튼은 하나만 눌리게 하였습니다. 완료된 채팅 Cell은 흐리게 설정하였습니다.
    
    이슈 유형: #9

--- Duckmelang/Duckmelang/ViewControllers/ChatViewController.swift
+++ Duckmelang/Duckmelang/ViewControllers/ChatViewController.swift
@@ -8,21 +8,66 @@
 import UIKit
 
 class ChatViewController: UIViewController {
+    var selectedTag: Int = 0
 
+    let data1 = ChatModel.dummy()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.navigationItem.title = "채팅"
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont.aritaSemiBoldFont(ofSize: 18)]
+        
+        self.view = chatView
+        setupDelegate()
+        setupAction()
+        updateBtnSelected()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private lazy var chatView: ChatView = {
+        let view = ChatView()
+        return view
+    }()
+    
+    private func setupDelegate() {
+        chatView.chatTableView.dataSource = self
+        chatView.chatTableView.delegate = self
     }
-    */
+    
+    private func setupAction() {
+        chatView.allBtn.addTarget(self, action: #selector(clickBtn), for: .touchUpInside)
+        chatView.ongoingBtn.addTarget(self, action: #selector(clickBtn), for: .touchUpInside)
+        chatView.doneBtn.addTarget(self, action: #selector(clickBtn), for: .touchUpInside)
+    }
+    
+    @objc func clickBtn(_ sender: UIButton) {
+        selectedTag = sender.tag
+        updateBtnSelected()
+    }
+    
+    private func updateBtnSelected() {
+        for btn in [chatView.allBtn, chatView.ongoingBtn, chatView.doneBtn] {
+            if btn.tag == selectedTag {
+                btn.isSelected = true
+            } else {
+                btn.isSelected = false
+            }
+        }
+    }
+}
 
+extension ChatViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data1.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ChatCell.identifier, for: indexPath) as? ChatCell else {
+            return UITableViewCell()
+        }
+        
+        cell.configure(model: data1[indexPath.row])
+        
+        return cell
+    }
 }
