c39e875cd8c971b3c78d46ff2d02f1bd51450dcd
yeon <rladusdn56@gmail.com>
2025-01-20 13:31:27 +0900

    Feat: BaseView로 이동 연결

--- Duckmelang/Duckmelang/Login/ViewController/LoginViewController.swift
+++ Duckmelang/Duckmelang/Login/ViewController/LoginViewController.swift
@@ -8,22 +8,47 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupView()
+        self.view = loginView
         self.title = "로그인"
         
         // 네비게이션 바 타이틀 폰트 설정
         if let navigationController = self.navigationController {
             navigationController.navigationBar.titleTextAttributes = [
-                NSAttributedString.Key.font: UIFont.aritaSemiBoldFont(ofSize: 18)
+                NSAttributedString.Key.font: UIFont
+                    .aritaSemiBoldFont(ofSize: 18)
             ]
         }
     }
-    private func setupView(){
-        let loginView = LoginView()
-        self.view = loginView
+    
+    // MARK: - Properties
+        
+    private lazy var loginView: LoginView = {
+        let view = LoginView()
+        view.loginButton
+            .addTarget(
+                self,
+                action: #selector(didTapLoginButton),
+                for: .touchUpInside
+            )
+        return view
+    }()
+
+    
+    
+    @objc private func didTapLoginButton() {
+        navigateToHomeView()
+        print("gotoMain")
+    }
+    
+    // MARK: - Navigation
+    
+    private func navigateToHomeView() {
+        let mainVC = BaseViewController()
+        mainVC.modalPresentationStyle = .fullScreen
+        present(mainVC, animated: true)
     }
 
 }
