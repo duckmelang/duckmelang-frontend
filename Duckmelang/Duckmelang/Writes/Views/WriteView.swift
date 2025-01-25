f1c5b9f6e34e0b26693cc17effd95fdab03752ec
yeon <rladusdn56@gmail.com>
2025-01-25 23:42:42 +0900

    Fix: 아이돌 선택 시 글자색 변경

--- Duckmelang/Duckmelang/Writes/Views/WriteView.swift
+++ Duckmelang/Duckmelang/Writes/Views/WriteView.swift
@@ -238,6 +238,7 @@ class WriteView: UIView {
 
     func updateSelectedCeleb(_ celeb: Celeb) {
         selectedCelebLabel.text = celeb.name
+        selectedCelebLabel.textColor = .black
     }
 }
 
