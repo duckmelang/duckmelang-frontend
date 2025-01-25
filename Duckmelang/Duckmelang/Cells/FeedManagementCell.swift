ccfd1dee849a9ecc65874b8a836bbab090cd7808
(nau) <finger9999@naver.com>
2025-01-26 08:58:45 +0900

    feat: 피드관리 delete 액션 추가
    셀 선택후 delete 버튼 누르면 삭제

---
+++ Duckmelang/Duckmelang/Cells/FeedManagementCell.swift
@@ -0,0 +1,138 @@
+//
+//  FeedManagementCell.swift
+//  Duckmelang
+//
+//  Created by KoNangYeon on 1/25/25.
+//
+
+import UIKit
+
+class FeedManagementCell: UITableViewCell {
+    static let identifier = "FeedManagementCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.contentView.layoutIfNeeded()
+        self.userImage.layer.cornerRadius = self.userImage.frame.width / 2
+        self.contentView.frame = self.contentView.frame.inset(by: UIEdgeInsets(top: 6, left: 16, bottom: 6, right: 16))
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.selectionStyle = .none
+        self.setView()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.postImage.image = nil
+        self.postTitle.text = nil
+        self.EventTypeDate.text = nil
+        self.userImage.image = nil
+        self.userName.text = nil
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let postImage = UIImageView().then {
+        $0.layer.borderWidth = 1
+        $0.layer.borderColor = UIColor.grey300?.cgColor
+        $0.layer.cornerRadius = 5
+        $0.clipsToBounds = true
+        $0.backgroundColor = .grey200
+    }
+    
+    let postTitle = UILabel().then {
+        $0.font = .ptdRegularFont(ofSize: 16)
+        $0.textColor = .grey900
+        $0.textAlignment = .left
+        $0.numberOfLines = 1
+    }
+    
+    let EventTypeDate = UILabel().then {
+        $0.font = .ptdRegularFont(ofSize: 12)
+        $0.textColor = .grey600
+        $0.textAlignment = .left
+    }
+    
+    let userImage = UIImageView().then {
+        $0.clipsToBounds = true
+        $0.backgroundColor = .grey200
+    }
+    
+    let userName = UILabel().then {
+        $0.font = .ptdRegularFont(ofSize: 12)
+        $0.textColor = .grey600
+        $0.textAlignment = .left
+    }
+    
+    let selectBtn = UIButton().then {
+        $0.setImage(.noSelect, for: .normal)
+        $0.setImage(.select, for: .selected)
+    }
+    
+    private func setView() {
+        [
+            postImage,
+            postTitle,
+            EventTypeDate,
+            userImage,
+            userName,
+            selectBtn,
+        ].forEach {
+            contentView.addSubview($0)
+        }
+        
+        postImage.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview().inset(6)
+            $0.leading.equalToSuperview().inset(16)
+            $0.width.height.equalTo(94)
+        }
+        
+        postTitle.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(12)
+            $0.leading.equalTo(postImage.snp.trailing).offset(12)
+        }
+        
+        EventTypeDate.snp.makeConstraints {
+            $0.top.equalTo(postTitle.snp.bottom).offset(6)
+            $0.leading.equalTo(postTitle.snp.leading)
+        }
+        
+        userImage.snp.makeConstraints {
+            $0.leading.equalTo(postTitle.snp.leading)
+            $0.bottom.equalToSuperview().offset(-7)
+            $0.width.height.equalTo(16)
+        }
+        
+        userName.snp.makeConstraints {
+            $0.centerY.equalTo(userImage.snp.centerY)
+            $0.leading.equalTo(userImage.snp.trailing).offset(6)
+        }
+        
+        selectBtn.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(6)
+            $0.trailing.equalToSuperview().inset(16)
+        }
+    }
+    
+    public func configure(model: FeedManagementModel) {
+        self.postImage.image = model.postImage
+        self.postTitle.text = model.postTitle
+        self.EventTypeDate.text = "\(model.EventType) | \(model.EventDate)"
+        self.userImage.image = model.userImage
+        self.userName.text = model.userName
+        
+        self.selectBtn.isSelected = model.isSelected
+        self.selectBtn.setImage(
+            model.isSelected ? UIImage(resource: .select) : UIImage(resource: .noSelect),
+            for: .normal
+        )
+    }
+}
--- Duckmelang/Duckmelang/MyPage/ViewController/Profile/FeedManagementViewController.swift
+++ Duckmelang/Duckmelang/MyPage/ViewController/Profile/FeedManagementViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class FeedManagementViewController: UIViewController {
+    
+    var data = FeedManagementModel.dummy()
+    
+    var selectedIndices: Set<IndexPath> = [] // 선택된 셀의 indexPath를 저장
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +20,7 @@ class FeedManagementViewController: UIViewController {
         
         navigationController?.isNavigationBarHidden = true
         
+        setupDelegate()
         setupAction()
     }
     
@@ -31,8 +36,82 @@ class FeedManagementViewController: UIViewController {
         self.presentingViewController?.dismiss(animated: false)
     }
     
+    @objc
+    private func deleteBtnDidTap() {
+        guard !selectedIndices.isEmpty else {
+            // 선택된 셀이 없을 경우
+            print("No selected row")
+            return
+        }
+        
+        // 선택된 indexPath들을 정렬하여 처리 (역순으로 삭제하면 index 문제가 발생하지 않음)
+        let sortedIndices = selectedIndices.sorted { $0.row > $1.row }
+        
+        // 데이터 모델에서 제거
+        for indexPath in sortedIndices {
+            data.remove(at: indexPath.row)
+        }
+        
+        // 테이블 뷰에서 제거
+        feedManagementView.postView.deleteRows(at: sortedIndices, with: .automatic)
+        
+        // 선택 상태 초기화
+        selectedIndices.removeAll()
+    }
+    
+    private func setupDelegate() {
+        feedManagementView.postView.dataSource = self
+        feedManagementView.postView.delegate = self
+    }
+    
     private func setupAction() {
         feedManagementView.backBtn.addTarget(self, action: #selector(backBtnDidTap), for: .touchUpInside)
         feedManagementView.finishBtn.addTarget(self, action: #selector(finishBtnDidTap), for: .touchUpInside)
+        feedManagementView.deleteBtn.addTarget(self, action: #selector(deleteBtnDidTap), for: .touchUpInside)
+    }
+}
+
+extension FeedManagementViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: FeedManagementCell.identifier, for: indexPath) as? FeedManagementCell else {
+            return UITableViewCell()
+        }
+        cell.configure(model: data[indexPath.row])
+        
+        // 셀이 선택된 상태인지 확인
+        if selectedIndices.contains(indexPath) {
+            cell.selectBtn.isSelected = true
+            cell.selectBtn.setImage(.select, for: .normal)
+            cell.contentView.backgroundColor = .grey100
+        } else {
+            cell.selectBtn.isSelected = false
+            cell.selectBtn.setImage(.noSelect, for: .normal)
+            cell.contentView.backgroundColor = .clear
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) as? FeedManagementCell {
+            // 선택된 셀을 토글
+            if selectedIndices.contains(indexPath) {
+                // 이미 선택된 경우, 선택 해제
+                selectedIndices.remove(indexPath)
+                cell.selectBtn.isSelected = false
+                cell.selectBtn.setImage(.noSelect, for: .normal)
+                cell.contentView.backgroundColor = .clear
+            } else {
+                // 새로 선택된 경우
+                selectedIndices.insert(indexPath)
+                cell.selectBtn.isSelected = true
+                cell.selectBtn.setImage(.select, for: .normal)
+                cell.contentView.backgroundColor = .grey100
+            }
+        }
     }
 }
