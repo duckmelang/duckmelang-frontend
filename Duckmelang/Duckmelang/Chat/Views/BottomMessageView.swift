60f3800136302e5ca4cf50073013e32f830c4db8
minyoy <65756020+minyoy@users.noreply.github.com>
2025-01-27 21:34:58 +0900

    Feat: 채팅 UI 생성 (#32)
    * Fix: 패키지 설정
    
    Fix: 패키지 설정
    
    중복 패키지를 제거했습니다.
    
    이슈 유형: #31
    
    * Feat: 메세지 셀 구현
    
    Feat: 메세지 셀 구현
    
    콜렉션 뷰에서 쓰일 내 메세지 셀과, 상대 메세지 셀을 구현했습니다.
    
    이슈 유형: #31
    
    * Feat: 메세지 상단뷰와 하단뷰 구현
    
    Feat: 메세지 상단뷰와 하단뷰 구현
    
    메세지뷰의 상단뷰와 하단뷰를 구현했습니다.
    
    이슈 유형: #31
    
    * Feat: 채팅 뷰와 뷰컨 이동 및 수정
    
    Feat: 채팅 뷰와 뷰컨 이동 및 수정
    
    메세지뷰를 띄울 수 있게 수정했으며, 파일 위치를 이동시켰습니다.
    
    이슈 유형: #31
    
    * Feat: 메세지 뷰 구현
    
    Feat: 메세지 뷰 구현
    
    전체적인 메세지 뷰를 구현했습니다.
    
    이슈 유형: #31

---
+++ Duckmelang/Duckmelang/Chat/Views/BottomMessageView.swift
@@ -0,0 +1,103 @@
+//
+//  BottomMessageView.swift
+//  Duckmelang
+//
+//  Created by 주민영 on 1/27/25.
+//
+
+import UIKit
+
+class BottomMessageView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .clear
+        setupView()
+        setupIncompleteView()
+//        setupCompleteView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private lazy var main = UIView().then {
+        $0.backgroundColor = .white
+    }
+    
+    private lazy var underline = UIView().then {
+        $0.backgroundColor = UIColor.grey200
+    }
+    
+    let messageTextField = UITextField().then {
+        $0.placeholder = "메시지 보내기"
+        $0.font = .ptdRegularFont(ofSize: 15)
+        $0.borderStyle = .none
+        $0.backgroundColor = .grey200
+        $0.textColor = .black
+        $0.layer.cornerRadius = 20
+        
+        $0.leftView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 30, height: 0.0))
+        $0.leftViewMode = .always
+        $0.rightView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 30, height: 0.0))
+        $0.rightViewMode = .always
+    }
+    
+    let sendBtn = UIButton().then {
+        $0.setImage(UIImage(named: "Send"), for: .normal)
+    }
+    
+    private func setupView() {
+        addSubview(main)
+        
+        [
+            underline,
+            messageTextField,
+        ].forEach {
+            main.addSubview($0)
+        }
+        
+        main.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.height.equalTo(60)
+        }
+        
+        underline.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(1)
+        }
+    }
+    
+    private func setupIncompleteView() {
+        main.addSubview(sendBtn)
+        
+        messageTextField.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(12)
+            $0.leading.equalToSuperview().offset(16)
+            $0.trailing.equalToSuperview().offset(-48)
+            $0.bottom.equalToSuperview().offset(-12)
+        }
+        
+        sendBtn.snp.makeConstraints {
+            $0.centerY.equalTo(messageTextField.snp.centerY)
+            $0.leading.equalTo(messageTextField.snp.trailing).offset(8)
+            $0.width.height.equalTo(24)
+        }
+    }
+    
+    private func setupCompleteView() {
+        messageTextField.isEnabled = false
+        messageTextField.isUserInteractionEnabled = false
+        messageTextField.text = "메시지를 보낼 수 없습니다."
+        messageTextField.textColor = .grey100
+        messageTextField.backgroundColor = .grey400
+        
+        messageTextField.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(12)
+            $0.leading.equalToSuperview().offset(20)
+            $0.trailing.equalToSuperview().offset(-20)
+            $0.bottom.equalToSuperview().offset(-12)
+            $0.width.greaterThanOrEqualTo(343)
+        }
+    }
+}
