ec102c611cff7b9339e2d8a7e6de413bcba2c2be
minyoy <65756020+minyoy@users.noreply.github.com>
2025-01-31 01:13:09 +0900

    Feat: API 초기셋팅
    Feat: API 초기셋팅
    
    API 호출을 위한 파일 생성, 도메인 설정, CommonResponse 생성
    
    이슈 유형: #37

---
+++ Duckmelang/Duckmelang/Network/Endpoint.swift
@@ -0,0 +1,66 @@
+//
+//  Endpoint.swift
+//  Duckmelang
+//
+//  Created by 주민영 on 1/28/25.
+//
+
+import UIKit
+import Moya
+
+public enum AllEndpoint {
+    case getMyPosts(memberId: Int, page: Int)
+    case getBookmarks(memberId: Int, page: Int)
+    case getProfileImage(memberId: Int, page: Int)
+}
+
+extension AllEndpoint: TargetType {
+    public var baseURL: URL {
+        switch self {
+        case .getMyPosts(_, _):
+            guard let url = URL(string: API.postURL) else {
+                fatalError("baseURL 오류")
+            }
+            return url
+        case .getBookmarks(_, _), .getProfileImage(_, _):
+            guard let url = URL(string: API.baseURL) else {
+                fatalError("baseURL 오류")
+            }
+            return url
+        }
+    }
+    
+    public var path: String {
+        switch self {
+        case .getMyPosts(_, _):
+            return "/my"
+        case .getBookmarks(let memberId, _):
+            return "/bookmarks/\(memberId)"
+        case .getProfileImage(_, _):
+            return "/mypage/profile/image/"
+        }
+    }
+    
+    public var method: Moya.Method {
+        switch self {
+        default:
+            return .get
+        }
+    }
+    
+    public var task: Moya.Task {
+        switch self {
+        case .getMyPosts(let memberId, let page), .getProfileImage(let memberId, let page):
+            return .requestParameters(parameters: ["memberId": memberId, "page": page], encoding: URLEncoding.queryString)
+        case .getBookmarks(_, let page):
+            return .requestParameters(parameters: ["page": page], encoding: URLEncoding.queryString)
+        }
+    }
+    
+    public var headers: [String : String]? {
+        switch self {
+        default :
+            return ["Content-Type": "application/json"]
+        }
+    }
+}
