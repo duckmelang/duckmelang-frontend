d2c366287de8501efe6a20b61be13cf5c1a817bc
(nau) <finger9999@naver.com>
2025-01-31 13:44:19 +0900

    fix: 충돌 고침

--- Duckmelang/Duckmelang/Models/reviewModel.swift
+++ Duckmelang/Duckmelang/Models/reviewModel.swift
@@ -1,5 +1,5 @@
 //
-//  reviewModel.swift
+//  ReviewModel.swift
 //  Duckmelang
 //
 //  Created by KoNangYeon on 1/23/25.
@@ -7,19 +7,19 @@
 
 import UIKit
 
-struct reviewModel {
+struct ReviewModel {
     let nickname: String
     let gender: String
     let age: String
     let review: String
 }
 
-extension reviewModel {
-    static func dummy() -> [reviewModel] {
+extension ReviewModel {
+    static func dummy() -> [ReviewModel] {
         return [
-            reviewModel(nickname: "닉네임", gender: "여성", age: "나이", review: "엄청 친절하세요! 저랑 대화도 잘 통해서 좋았습니다:)"),
-            reviewModel(nickname: "닉네임", gender: "여성", age: "나이", review: "엄청 친절하세요! 저랑 대화도 잘 통해서 좋았습니다:)"),
-            reviewModel(nickname: "닉네임", gender: "여성", age: "나이", review: "엄청 친절하세요! 저랑 대화도 잘 통해서 좋았습니다:)")
+            ReviewModel(nickname: "닉네임", gender: "여성", age: "나이", review: "엄청 친절하세요! 저랑 대화도 잘 통해서 좋았습니다:)"),
+            ReviewModel(nickname: "닉네임", gender: "여성", age: "나이", review: "엄청 친절하세요! 저랑 대화도 잘 통해서 좋았습니다:)"),
+            ReviewModel(nickname: "닉네임", gender: "여성", age: "나이", review: "엄청 친절하세요! 저랑 대화도 잘 통해서 좋았습니다:)")
         ]
     }
 }
