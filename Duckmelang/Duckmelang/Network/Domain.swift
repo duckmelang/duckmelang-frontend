4a0311cc0fb51f0854a933e0c29bd56d18682a44
minyoy <65756020+minyoy@users.noreply.github.com>
2025-01-31 16:34:09 +0900

    Feat: API 초기셋팅 (#40)
    * Feat: API 초기셋팅
    
    Feat: API 초기셋팅
    
    API 호출을 위한 파일 생성, 도메인 설정, CommonResponse 생성
    
    이슈 유형: #37
    
    * Feat: 스크랩, 내 게시물 불러오기 API 호출 예시 구현
    
    Feat: 스크랩, 내 게시물 불러오기 API 호출 예시 구현
    
    테스트 및 예제로 스크랩 및 내 게시물 불러오기 API를 호출하는 함수를 만들어서 적용해놓았습니다.
    
    이슈 유형: #37

---
+++ Duckmelang/Duckmelang/Network/Domain.swift
@@ -0,0 +1,18 @@
+//
+//  Domain.swift
+//  Duckmelang
+//
+//  Created by 주민영 on 1/28/25.
+//
+
+import UIKit
+
+public struct API {
+    public static let baseURL = "http://13.125.217.231:8080"
+    public static let reviewURL = "\(baseURL)/reviews"
+    public static let requestURL = "\(baseURL)/requests"
+    public static let postURL = "\(baseURL)/posts"
+    public static let memberURL = "\(baseURL)/members"
+    public static let mypageURL = "\(baseURL)/mypage"
+    public static let profileURL = "\(baseURL)/profile"
+}
---
+++ Duckmelang/Duckmelang/Network/Response.swift
@@ -0,0 +1,27 @@
+//
+//  Response.swift
+//  Duckmelang
+//
+//  Created by 주민영 on 1/28/25.
+//
+
+import Foundation
+
+// 게시글 정보를 담는 구조체
+public struct PostDTO: Codable {
+    public let postId: Int
+    public let title: String
+    public let category: String
+    public let date: String
+    public let nickname: String
+    public let createdAt: String
+}
+
+public struct PostResponse: Codable {
+    let postList: [PostDTO]
+    let listSize: Int
+    let totalPage: Int
+    let totalElements: Int
+    let isFirst: Bool
+    let isLast: Bool
+}
