d02ca1e5b511ef0de5dc0d51b1696cf5685bb769
yeon <rladusdn56@gmail.com>
2025-02-01 19:59:20 +0900

    Feat: 검색 바 생성

--- Duckmelang/Duckmelang/Home/Views/SearchView.swift
+++ Duckmelang/Duckmelang/Home/Views/SearchView.swift
@@ -11,6 +11,29 @@ import SnapKit
 
 class SearchView: UIView {
     
+    private let searchTextField = UITextField().then {
+        $0.placeholder = "텍스트 입력"
+        $0.borderStyle = .roundedRect
+
+        // 왼쪽 패딩 추가
+        let leftPaddingView = UIView(frame: CGRect(x: 0, y: 0, width: 16, height: 44))
+        $0.leftView = leftPaddingView
+        $0.leftViewMode = .always
+
+        // 돋보기 아이콘 추가 (오른쪽)
+        let searchIcon = UIImageView(image: UIImage(systemName: "magnifyingglass"))
+        searchIcon.tintColor = .grey600
+        searchIcon.contentMode = .scaleAspectFit
+        searchIcon.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
+
+        let rightPaddingView = UIView(frame: CGRect(x: 0, y: 0, width: 40, height: 44))
+        searchIcon.center = CGPoint(x: rightPaddingView.frame.width / 2, y: rightPaddingView.frame.height / 2)
+        rightPaddingView.addSubview(searchIcon)
+
+        $0.rightView = rightPaddingView
+        $0.rightViewMode = .always
+    }
+    
     override init(frame: CGRect) {
             super.init(frame: frame)
         self.backgroundColor = UIColor.white
@@ -24,8 +47,16 @@ class SearchView: UIView {
         private func setupView() {
             
             [
+                searchTextField
             ].forEach {
                 addSubview($0)
+            }
+            
+            searchTextField.snp.makeConstraints{
+                $0.leading.equalToSuperview().offset(20)
+                $0.trailing.equalToSuperview().offset(-20)
+                $0.top.equalTo(safeAreaLayoutGuide).offset(10)
+                $0.height.equalTo(40)
             }
         }
     
