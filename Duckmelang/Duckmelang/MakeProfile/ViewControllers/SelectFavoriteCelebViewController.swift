099da187a3ae9eda9d46952b084e6097b3209130
yeon <rladusdn56@gmail.com>
2025-02-20 01:51:17 +0900

    Feat: NextStepHandler 부착

--- Duckmelang/Duckmelang/MakeProfile/ViewControllers/SelectFavoriteCelebViewController.swift
+++ Duckmelang/Duckmelang/MakeProfile/ViewControllers/SelectFavoriteCelebViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import Alamofire
 import Moya
 
-class SelectFavoriteCelebViewController: UIViewController, NextButtonUpdatable, MoyaErrorHandlerDelegate {
+class SelectFavoriteCelebViewController: UIViewController, NextButtonUpdatable, MoyaErrorHandlerDelegate, NextStepHandler {
     
     weak var nextButtonDelegate: NextButtonUpdatable?
     
