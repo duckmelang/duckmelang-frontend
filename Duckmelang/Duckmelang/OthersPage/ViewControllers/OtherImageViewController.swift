698e0eb9f9b81338f1164cb9f0ae66552f573e31
minyoy <65756020+minyoy@users.noreply.github.com>
2025-02-21 02:06:57 +0900

    Feat: 다른사람 이미지뷰 페이지네이션 적용
    Feat: 다른사람 이미지뷰 페이지네이션 적용
    
    다른사람 이미지뷰 페이지네이션 적용
    
    이슈 유형: #106

--- Duckmelang/Duckmelang/OthersPage/ViewControllers/OtherImageViewController.swift
+++ Duckmelang/Duckmelang/OthersPage/ViewControllers/OtherImageViewController.swift
@@ -10,11 +10,14 @@ import Moya
 
 class OtherImageViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     private let provider = MoyaProvider<OtherPageAPI>(plugins: [TokenPlugin(), NetworkLoggerPlugin(configuration: .init(logOptions: .verbose))])
-    
     private var profileImageData: [OtherImageData] = []
     
     var oppositeId: Int?
     var profileData: OtherProfileData?
+    
+    var isLoading = false   // 중복 로딩 방지
+    var isLastPage = false  // 마지막 페이지인지 여부
+    var currentPage = 0     // 현재 페이지 번호
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,21 +52,37 @@ class OtherImageViewController: UIViewController, UITableViewDelegate, UITableVi
         self.navigationController?.popViewController(animated: true)
     }
 
+    // MARK: - 프로필 이미지 목록 가져오기
     private func getOtherProfileImageAPI() {
-        provider.request(.getOtherProfileImage(memberId: self.oppositeId!, page: 0)) { result in
+        guard !isLoading && !isLastPage else { return } // 중복 호출 & 마지막 페이지 방지
+        isLoading = true
+        otherImageView.loadingIndicator.startLoading()
+        
+        provider.request(.getOtherProfileImage(memberId: self.oppositeId!, page: currentPage)) { result in
             switch result {
             case .success(let response):
-                self.profileImageData.removeAll()
-                let response = try? response.map(ApiResponse<OtherImageResponse>.self)
-                guard let result = response?.result?.profileImageList else { return }
-                self.profileImageData = result
-                print("다른 사람 이미지: \(self.profileImageData)")
-                
-                DispatchQueue.main.async {
-                    self.otherImageView.imageTableView.reloadData()
+                DispatchQueue.global().asyncAfter(deadline: .now() + 1.0) {
+                    let response = try? response.map(ApiResponse<OtherImageResponse>.self)
+                    guard let result = response?.result?.profileImageList else { return }
+                    guard let isLast = response?.result?.isLast else { return }
+                    self.profileImageData.append(contentsOf: result)
+                    print("다른 사람 이미지: \(self.profileImageData)")
+                    
+                    DispatchQueue.main.async {
+                        self.isLastPage = isLast
+                        self.isLoading = false
+                        self.otherImageView.loadingIndicator.stopLoading()
+                        self.otherImageView.imageTableView.reloadData()
+                        
+                        if isLast {
+                            self.otherImageView.imageTableView.tableFooterView = nil
+                        }
+                    }
                 }
             case .failure(let error):
                 print(error)
+                self.isLoading = false
+                self.otherImageView.loadingIndicator.stopLoading()
             }
         }
     }
@@ -92,5 +111,18 @@ class OtherImageViewController: UIViewController, UITableViewDelegate, UITableVi
 
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return section == 0 ? 0 : 10
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offsetY = scrollView.contentOffset.y
+        let contentHeight = scrollView.contentSize.height
+        let tableViewHeight = scrollView.frame.size.height
+
+        if offsetY > contentHeight - tableViewHeight * 2 {
+            if !isLoading && !isLastPage {
+                currentPage += 1
+                getOtherProfileImageAPI()
+            }
+        }
     }
 }
