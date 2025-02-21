c3d436071bdaa6f7b4691be7f8358e9a596ed37c
yeon <rladusdn56@gmail.com>
2025-02-21 12:03:50 +0900

    Feat: 회원가입 로직 완료

--- Duckmelang/Duckmelang/Network/Responses/LoginResponse.swift
+++ Duckmelang/Duckmelang/Network/Responses/LoginResponse.swift
@@ -59,6 +59,19 @@ public struct VerifyCodeResponse: Codable {
     let result: String?
 }
 
+public struct SignupResponse: Codable {
+    let isSuccess: Bool
+    let code: String
+    let message: String
+    let result: SignupResponseResult
+}
+
+struct SignupResponseResult: Codable {
+    let memberId: Int
+    let createdAt: String
+    let profileComplete: Bool
+}
+
 //닉네임 중복 확인 응답 모델
 struct NicknameCheckResponse: Decodable {
     let isSuccess: Bool
